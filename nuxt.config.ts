4ffe440f6170ef2ad8d2458b8501874d256d790b
Wyze00 <nathantanusubroto15@gmail.com>
2026-09-07 21:37:18 +0700

    Update Font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,15 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['./app/assets/css/main.css'],
+  app: {
+    head: {
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap' }
+      ]
+    }
+  },
   vite: {
     plugins: [
       tailwindcss(),
